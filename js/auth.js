d2b6e9790136c37177f2d0e292a958677f1c8212
Windus <huizhimrl@gmail.com>
2023-07-11 15:11:19 +0800

    update

---
+++ js/auth.js
@@ -0,0 +1,32 @@
+(() => {
+  const auth = () => {
+    const day = 60 * 60 * 24 * 1000;
+    const { is_post, lock, passwords, root } = window.AD_CONFIG;
+
+    if(is_post === false || lock === false) {
+      return;
+    }
+
+    let [password, expires] = atob(window.localStorage.getItem('auth')).split(':'),
+      now = new Date().getTime();
+
+    if(passwords.includes(password) && now < expires) {
+      return; 
+    }
+
+    password = prompt('输入您的名称小写全拼 (例如: 李三 => lisan)');
+    password = sha256(password || '');
+
+    if(passwords.includes(password)) {
+      expires = now + day * 3;
+      window.localStorage.setItem('auth', btoa(`${password}:${expires}`));
+    } else {
+      alert('您没有阅读权限');
+      window.location.href = root;
+    }
+  };
+
+
+  // article password auth
+  auth();
+})();
